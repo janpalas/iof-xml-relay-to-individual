49bc82a71f976e4443a4c537b41e2ff19203fa0f
honza <jan.palas@aginfo.cz>
2023-08-18 16:31:54 +0200

    Added tests for result transofrmation

--- src/IofXmlTransform.Tests/IofXmlManagerTests.cs
+++ src/IofXmlTransform.Tests/IofXmlManagerTests.cs
@@ -15,7 +15,75 @@ public class IofXmlManagerTests
 		//act
 		XDocument individualXml = IofXmlManager.FromRelayToIndividualResults(relayXml, 4);
 
-		individualXml.Save(@"C:\Devel\tes-result.xml");
+		//assert
+		XElement relayEvent = individualXml.Descendants(Namespace + "Event").First();
+		XElement individualEvent = individualXml.Descendants(Namespace + "Event").First();
+		Assert.AreEqual(relayEvent.ToString(), individualEvent.ToString());
+
+		List<XElement> relayClassResults = relayXml.Descendants(Namespace + "ClassResult").ToList();
+		List<XElement> individualClassResults = individualXml.Descendants(Namespace + "ClassResult").ToList();
+		Assert.AreEqual(relayClassResults.Count, individualClassResults.Count);
+
+		foreach (XElement relayClassResult in relayClassResults)
+		{
+			XElement relayClassInfo = relayClassResult.Descendants(Namespace + "Class").First();
+
+			string classId = relayClassInfo.Descendants(Namespace + "Id").First().Value;
+			string className = relayClassInfo.Descendants(Namespace + "Name").First().Value;
+
+			XElement? individualClassResult = individualClassResults.FirstOrDefault(x => x.Descendants(Namespace + "Class").Descendants(Namespace + "Name").First().Value == className);
+			Assert.IsNotNull(individualClassResult);
+
+			XElement individualClassInfo = individualClassResult.Descendants(Namespace + "Class").First();
+			Assert.AreEqual(classId, individualClassInfo.Descendants(Namespace + "Id").First().Value);
+
+			List<XElement> teamMemberResults = relayClassResults.Descendants(Namespace + "TeamMemberResult").ToList();
+			List<XElement> personResults = individualClassResults.Descendants(Namespace + "PersonResult").ToList();
+			Assert.AreEqual(teamMemberResults.Count, personResults.Count);
+
+			foreach (XElement teamMemberResult in teamMemberResults)
+			{
+				XElement? relayOrganization = teamMemberResult.Parent?.Descendants(Namespace + "Organisation").First();
+				Assert.IsNotNull(relayOrganization);
+
+				XElement teamPerson = teamMemberResult.Descendants(Namespace + "Person").First();
+				string teamPersonId = teamPerson.Descendants(Namespace + "Id").First(x => x.Attribute("type")?.Value == "QuickEvent").Value;
+
+				XElement? individualPersonResult = personResults.FirstOrDefault(x => x.Descendants(Namespace + "Person").Descendants(Namespace + "Id").First(y => y.Attribute("type")?.Value == "QuickEvent").Value == teamPersonId);
+				Assert.IsNotNull(individualPersonResult);
+
+				XElement individualPerson = individualPersonResult.Descendants(Namespace + "Person").First();
+				Assert.AreEqual(teamPerson.ToString(), individualPerson.ToString());
+
+				XElement? individualOrganisation = individualPersonResult.Descendants(Namespace + "Organisation").FirstOrDefault();
+				Assert.IsNotNull(individualOrganisation);
+				Assert.AreEqual(2, individualOrganisation.Descendants().Count());
+				Assert.AreEqual(relayOrganization.Descendants(Namespace + "Id").First().ToString(), individualOrganisation.Descendants(Namespace + "Id").First().ToString());
+				Assert.AreEqual(relayOrganization.Descendants(Namespace + "Name").First().ToString(), individualOrganisation.Descendants(Namespace + "Name").First().ToString());
+
+				XElement? individualResult = individualPersonResult.Descendants(Namespace + "Result").FirstOrDefault();
+				XElement relayResult = teamMemberResult.Descendants(Namespace + "Result").First();
+				
+				Assert.IsNotNull(individualResult);
+				Assert.AreEqual(relayResult.Descendants(Namespace + "StartTime").First().Value, individualResult.Descendants(Namespace + "StartTime").First().Value);
+				Assert.AreEqual(relayResult.Descendants(Namespace + "FinishTime").First().Value, individualResult.Descendants(Namespace + "FinishTime").First().Value);
+				Assert.AreEqual(relayResult.Descendants(Namespace + "Time").First().Value, individualResult.Descendants(Namespace + "Time").First().Value);
+				Assert.AreEqual(relayResult.Descendants(Namespace + "Status").First().Value, individualResult.Descendants(Namespace + "Status").First().Value);
+
+				XElement timeBehind = individualResult.Descendants(Namespace + "TimeBehind").First();
+				Assert.AreEqual(relayResult.Descendants(Namespace + "TimeBehind").First().Value, timeBehind.Value);
+				Assert.IsNull(timeBehind.Attribute("type"));
+
+				XElement position = individualResult.Descendants(Namespace + "Position").First();
+				Assert.AreEqual(relayResult.Descendants(Namespace + "Position").First().Value, position.Value);
+				Assert.IsNull(position.Attribute("type"));
+
+				List<XElement> relaySplitTimes = relayResult.Descendants(Namespace + "SplitTime").ToList();
+				List<XElement> individualSplitTimes = individualResult.Descendants(Namespace + "SplitTime").ToList();
+				Assert.AreEqual(relaySplitTimes.Count, individualSplitTimes.Count);
+				Assert.AreEqual(string.Join(';', relaySplitTimes.Select(x => x.ToString())), string.Join(';', individualSplitTimes.Select(x => x.ToString())));
+			}
+		}
 	}
 
 	[Test]
@@ -28,6 +96,9 @@ public class IofXmlManagerTests
 		XDocument relayEntries = IofXmlManager.FromIndividualToRelayEntries(individualEntries);
 
 		relayEntries.Save(@"C:\Devel\relay-entries.xml");
+
+		//assert
+		Assert.AreEqual("Relay", relayEntries.Descendants(Namespace + "Event").First().Descendants(Namespace + "Form").First().Value);
 
 		List<XElement> personEntries = individualEntries.Descendants(Namespace + "PersonEntry").ToList();
 		List<XElement> teamEntries = relayEntries.Descendants(Namespace + "TeamEntry").ToList();
